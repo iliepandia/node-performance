9b87eabb2cc7d31b2ec577eb6c688053e1f77f51
Ilie Pandia <ilie.pandia@gmail.com>
2023-09-19 09:36:21 -0700

    improved the client to take job as a parameter and the promises recreate themselves on success or failure

--- stressfull-client.js
+++ stressfull-client.js
@@ -1,31 +1,45 @@
-console.log( "Starting the stressful client" );
+console.log("Starting the stressful client");
 
+const concurrentPromises = parseInt(process.argv[2], 10) || 5;
 
-const concurrentRuns = 5;
-
-let jobs = [];
-
-let makePromise = ( i ) => {
-    return new Promise( (resolve, reject ) => {
-        setTimeout( () => {
-            //install a new promise where one has been resolved
-            jobs[i] = makePromise( i );
-            resolve("P:" + i + " is done");
-        }, (Math.random() * 3 + 2) * 100 );
-    } );
-};
-
-for(let i = 0; i < concurrentRuns; i++ ){
-
-    let p = makePromise( i );
-    jobs.push( p );
+if (isNaN(concurrentPromises) || concurrentPromises <= 0) {
+    console.error("Invalid number of concurrent promises. Exiting.");
+    process.exit(1);
 }
 
-let reRun = ( result ) => {
-    console.log( "One of the promises returned ", result );
-    Promise.race(jobs).then(reRun);
+console.log("Starting the stressful client with ", concurrentPromises, " jobs.");
+
+
+const jobStatus = [];
+
+//print the jobs status in a pretty way
+//I want to see that everyone is "busy"
+const printStatus = () => {
+    console.log(jobStatus.join(''));
+}
+
+const makePromise = (i) => {
+    return new Promise((resolve) => {
+        jobStatus[i] = "1";
+        setTimeout(() => {
+            jobStatus[i] = "0";
+            resolve("P:" + i + " is done");
+        }, (Math.random() * 3 + 2) * 100);
+    }).then((result) => {
+        // Log and restart the promise
+        printStatus();
+        return makePromise(i);
+    }).catch((error) => {
+        jobStatus[i] = "x";
+        printStatus();
+        return makePromise(i);
+    });
 };
 
-Promise.race(jobs).then( reRun );    
-    
+// Initialize the job promises
+for (let i = 0; i < concurrentPromises; i++) {
+    makePromise(i);
+}
 
+// You don't need to run anything after initialization because 
+// each promise will keep re-creating itself after it resolves.
